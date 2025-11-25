7b37da4adebef0cc5b19ecfa94c37ca26c09baae
sudenazz1 <sudenazlekesiz@gmail.com>
2025-11-25 16:45:00 +0300

    dinamik agirlik hesaplama formulu eklendi

--- edge.py
+++ edge.py
@@ -1,5 +1,17 @@
+EDGE.PY
+import math
+
 class Edge:
     def __init__(self, source_node, target_node):
         self.source = source_node
         self.target = target_node
-        self.weight = 1.0
+        self.weight = self.calculate_weight()
+
+    def calculate_weight(self):
+        # PDF Formülü
+        diff_aktif = self.source.aktiflik - self.target.aktiflik
+        diff_etkilesim = self.source.etkilesim - self.target.etkilesim
+        diff_baglanti = self.source.baglanti_sayisi - self.target.baglanti_sayisi
+        
+        inside_sqrt = (diff_aktif**2) + (diff_etkilesim**2) + (diff_baglanti**2)
+        return 1 + math.sqrt(inside_sqrt)
