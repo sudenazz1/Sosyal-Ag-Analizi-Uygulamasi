f2cca84769870f4168777aa284f7a7cc6cc1c9f7
sudenazz1 <sudenazlekesiz@gmail.com>
2025-12-31 19:35:35 +0300

    baglanti sayisi kaldirildi dinamik hesaplanacak

--- node.py
+++ node.py
@@ -1,17 +1,23 @@
-NODE.PY
 class Node:
-    def __init__(self, user_id, name, aktiflik, etkilesim, baglanti_sayisi):
+    """
+    Sosyal ağ düğümünü temsil eden sınıf.
+    baglanti_sayisi artık dinamik olarak graph'tan hesaplanacak.
+    """
+    def __init__(self, user_id, name, aktiflik, etkilesim):
         self.id = int(user_id)
         self.name = name
         self.aktiflik = float(aktiflik)      # Ozellik I
         self.etkilesim = float(etkilesim)    # Ozellik II
-        self.baglanti_sayisi = int(baglanti_sayisi) # Ozellik III
+        # baglanti_sayisi kaldırıldı - Graph sınıfı tarafından dinamik hesaplanacak
     
     def to_dict(self):
+        """Node'u JSON formatına çevir"""
         return {
-            "id": self.id, "name": self.name,
-            "aktiflik": self.aktiflik, "etkilesim": self.etkilesim,
-            "baglanti_sayisi": self.baglanti_sayisi
+            "id": self.id,
+            "name": self.name,
+            "aktiflik": self.aktiflik,
+            "etkilesim": self.etkilesim
         }
     
-    
+    def __repr__(self):
+        return f"Node(id={self.id}, name='{self.name}')"
